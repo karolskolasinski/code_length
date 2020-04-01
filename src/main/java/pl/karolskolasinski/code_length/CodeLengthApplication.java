7702639378c9092564a584db417597e21c5c763d
Karol Skolasiński <karolskolasinski@gmail.com>
2020-04-01 19:03:24 +0200

    initial commit

---
+++ src/main/java/pl/karolskolasinski/code_length/CodeLengthApplication.java
@@ -0,0 +1,13 @@
+package pl.karolskolasinski.code_length;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLengthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeLengthApplication.class, args);
+    }
+
+}
